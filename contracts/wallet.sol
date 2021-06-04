8fc1ea0bdc34dfc86f9743b56ea595842c0dd059
Kazuhiro Nakagawa <kazuhiro.nakagawa@okcoin.net>
2021-06-04 17:05:51 +0900

    Add contracts

---
+++ contracts/wallet.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.5.0;
+
+import "./charity.sol";
+
+contract Wallet {
+    Charity charity;
+
+    constructor(address payable charityAddress) public payable {
+        charity = Charity(charityAddress);
+    }
+
+    function donate(uint256 amount) public {
+        charity.donate.value(amount)();
+    }
+
+    function getDonationBalance() public view returns (uint256) {
+        return charity.getBalance();
+    }
+
+    function withdraw() public {
+        charity.withdraw();
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function() external payable {
+        charity.withdraw();
+    }
+}
